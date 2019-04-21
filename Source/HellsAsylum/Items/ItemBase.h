e05030fb4537866243e3eb6919d92cd31a544bc7
Brandon Jones <bcjones1@fullsail.edu>
2019-04-20 21:43:13 -0400

    Implemented events for tutorial message, objective updates Added new data tables for hammer, hornet

---
+++ Source/HellsAsylum/Items/ItemBase.h
@@ -0,0 +1,57 @@
+// Copyright 2019 Eternal Starlight Studios ALL RIGHTS RESERVED
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ItemBase.generated.h"
+
+UENUM(BlueprintType)
+enum EItemType
+{
+	IT_None UMETA(DisplayName = "None"),
+	IT_Key UMETA(DisplayName = "Key"),
+	IT_StoryKey UMETA(DisplayName = "Story Key"),
+	IT_Health UMETA(DisplayName = "Health Recovery"),
+	IT_Shield UMETA(DisplayName = "Shield Recovery"),
+	IT_Aragon UMETA(DisplayName = "Aragon Recovery"),
+	IT_PowerBoost UMETA(DisplayName = "Suit Cooldown Boost")
+
+};
+
+UCLASS()
+class HELLSASYLUM_API AItemBase : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AItemBase();
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Item|Properties")
+		bool bIsKeyItem;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Item|Properties")
+		bool bIsItemStackable;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|Properties")
+		float HealthRecoverAmount = 0.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|Properties")
+		float ShieldRecoverAmount = 0.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|Properties")
+		float AragonRecoverAmount = 0.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item|Properties")
+		float CooldownModifierAmount = 0.0f;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
