0f8511ad8b9500ab4d6c787fb409f54d90b62097
Brandon Jones <bcjones1@fullsail.edu>
2019-07-05 22:28:39 -0400

    Update data tables Update Player HUD

--- Build/Android/src/com/ess/HA/Android/OBBData.java
+++ Build/Android/src/com/ess/HA/Android/OBBData.java
@@ -19,7 +19,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "2", // the version of the APK that the file was uploaded against
-406145821L // the length of the file in bytes
+443567624L // the length of the file in bytes
 )
 };
 };
